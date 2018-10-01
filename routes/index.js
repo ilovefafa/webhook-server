373724d72b871c14d1b7adef47e382a0e48cc4e5
ilovefafa <3373198511@qq.com>
2018-10-01 15:31:41 +0800

    done website-hook

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,10 @@ router.get('/', function (req, res, next) {
 
 router.post('/payload/yineng-website-back-end', function (req, res, next) {
   let respone = req.body
-  if (respone.commits.length > 0) {
     shell.cd('/usr/website/yineng-website-back-end')
-    // console.log(shell.exec('git pull'))
-    // console.log(shell.exec('git install'))
-    // console.log(shell.exec('pm2 restart yineng-website'))
     let pull = shell.exec('git pull')
-    let install = shell.exec('git install')
+    let install = shell.exec('npm install')
     let restart = shell.exec('pm2 restart yineng-website')
-  }
   res.json({
     pull,
     install,
