5849ffd216fe207babc344bd298dba7de1cd1698
ilovefafa <3373198511@qq.com>
2018-10-01 16:20:00 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,58 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/payload/yineng-website-back-end', function (req, res, next) {
-  let respone = req.body
-    shell.cd('/usr/website/yineng-website-back-end')
+function createHook(repo, appName) {
+  if (!appName) appName = repo
+  router.post(`/payload/${repo}`, function (req, res, next) {
+    shell.cd(`/usr/website/${repo}`)
     let pull = shell.exec('git pull')
     let install = shell.exec('npm install')
-    let restart = shell.exec('pm2 restart yineng-website')
-  res.json({
-    pull,
-    install,
-    restart
+    let restart = shell.exec(`pm2 restart ${appName}`)
+    let commandResults = {
+      pull,
+      install,
+      restart,
+    }
+    let isError = Object.keys(commandResults).some((item) => {
+      return commandResults[item].code !== 0
+    })
+    if (isError) {
+      res.json({
+        message: 'error'
+      })
+    } else {
+      res.json({
+        message: 'done'
+      })
+    }
   });
-});
+}
+
+createHook('yineng-website-back-end', 'yineng-website')
+createHook('webhook-server')
+
+// router.post('/payload/yineng-website-back-end', function (req, res, next) {
+//   shell.cd('/usr/website/yineng-website-back-end')
+//   let pull = shell.exec('git pull')
+//   let install = shell.exec('npm install')
+//   let restart = shell.exec('pm2 restart yineng-website')
+//   let commandResults = {
+//     pull,
+//     install,
+//     restart,
+//   }
+//   let isError = Object.keys(commandResults).some((item) => {
+//     return commandResults[item].code !== 0
+//   })
+//   if (isError) {
+//     res.json({
+//       message: 'error'
+//     })
+//   } else {
+//     res.json({
+//       message: 'done'
+//     })
+//   }
+// });
 
 module.exports = router;
